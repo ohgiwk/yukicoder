581708590d45e6991193e1af0243d197cf34bc0b
渡辺圭哉 <Keiya@Keiya.local>
2020-03-21 02:09:08 +0900

    fix C# code for running yukicoder

--- 1003_サイコロの実装(1)/main.cs
+++ 1003_サイコロの実装(1)/main.cs
@@ -1,12 +1,23 @@
-
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-// String input = Console.ReadLine();
-String input = "6";
-var n = Convert.ToInt32(input);
-// var n = 6;
-IEnumerable<int> l = Enumerable.Range(1, n).Select(i => i % 6 + 1);
-IEnumerable<int> l2 = Enumerable.Range(1, 6).Select(i => l.Where(j => i == j).Count());
-bool result = l2.All(i => i == l2.ElementAt(0));
+public class Program
+{
+    public static void Main()
+    {
+        String input = Console.ReadLine();
+        // String input = "6";
+        int n = Convert.ToInt32(input);
+        // var n = 6;
+        IEnumerable<int> l = Enumerable.Range(1, n).Select(i => i % 6 + 1);
+        IEnumerable<int> l2 = Enumerable.Range(1, 6).Select(i => l.Where(j => i == j).Count());
+        bool result = l2.All(i => i == l2.ElementAt(0));
 
-Console.WriteLine(result? "Yes" : "No");
+        Console.WriteLine(result ? "Yes" : "No");
+    }
+}
+
+
+// Program.Main();
--- 998_Four_Integers/main.cs
+++ 998_Four_Integers/main.cs
@@ -1,11 +1,24 @@
+
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-String input = Console.ReadLine();
-// String input = "1 2 3 4";
-IEnumerable<int> l1 = input.Split(" ")
-                      .Select(s => Convert.ToInt32(s))
-                      .OrderBy(i => i);
-IEnumerable<int> l2 = Enumerable.Range(l1.Min(), l1.Max());
+public class Program
+{
+    public static void Main()
+    {
+        String input = Console.ReadLine();
+        // String input = "1 2 3 4";
+        IEnumerable<int> l1 = input.Split(" ")
+                              .Select(s => Convert.ToInt32(s))
+                              .OrderBy(i => i);
+        IEnumerable<int> l2 = Enumerable.Range(l1.Min(), l1.Max());
 
-Console.WriteLine(l1.SequenceEqual(l2)? "Yes" : "No");
+        Console.WriteLine(l1.SequenceEqual(l2) ? "Yes" : "No");
+    }
 
+}
+
+
+// Program.Main();
