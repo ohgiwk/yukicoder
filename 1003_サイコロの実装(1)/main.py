a4504260d175bd722e8c65f63280129fa14f391b
渡辺圭哉 <Keiya@Keiya.local>
2020-03-26 00:13:08 +0900

    fix:  inject IOProvider

--- 1003_サイコロの実装(1)/main.py
+++ 1003_サイコロの実装(1)/main.py
@@ -1,13 +1,52 @@
+from abc import ABCMeta, abstractmethod
 
-def main(i: str):
-    n = int(i)
-    l1: list = [x % 6 + 1 for x in range(1, n + 1)]
-    match = lambda x, l: list(filter(lambda y: y == x, l))
-    l2: list = [len(match(x, l1)) for x in range(1, 7)]
-    result: bool = all([ x == l2[0] for x in l2 ])
+class IOProvider(metaclass=ABCMeta):
+    @abstractmethod
+    def input(self) -> str:
+        pass
+    @abstractmethod
+    def output(self, s: str) -> None:
+        pass
 
-    print("Yes" if result else "No")
+class StandardIOProvider(IOProvider):
+    def input(self) -> str:
+        return input()
+
+    def output(self, s: str) -> None:
+        print(s)
+
+class TestIOProvider(IOProvider):
+    def input(self) -> str:
+        return "6"
+
+    def output(self, s: str) -> None:
+        print(s)
+
+class Program:
+    def __init__(self, provider: IOProvider):
+        self.io = provider
+
+    def read(self) -> str:
+        return self.io.input()
+
+    def write(self, s: str):
+        self.io.output(s)
+
+    def main(self) -> None:
+        i = self.read()
+
+        n = int(i)
+        l1: list = [x % 6 + 1 for x in range(1, n + 1)]
+        match = lambda x, l: list(filter(lambda y: y == x, l))
+        l2: list = [len(match(x, l1)) for x in range(1, 7)]
+        result: bool = all([ x == l2[0] for x in l2 ])
+
+        self.write("Yes" if result else "No")
+
+
+def main():
+    provider = TestIOProvider()
+    Program(provider).main()
 
 if __name__ == '__main__':
-    i: str = input()
-    main(i)
+    main()
