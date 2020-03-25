02086c4b1cde27df021c4fd38b2468eb700def4a
渡辺圭哉 <Keiya@Keiya.local>
2020-03-26 00:10:43 +0900

    add No.1003 Dlang Program

---
+++ 1003_サイコロの実装(1)/main.d
@@ -0,0 +1,87 @@
+import std.stdio;
+import std.range : iota;
+import std.algorithm : map, filter, all;
+import std.conv;
+import std.array;
+
+private:
+interface IOProvider
+{
+  string input();
+  void output(string);
+}
+
+class StandardIOProvider : IOProvider
+{
+  string input()
+  {
+    return "";
+  }
+
+  void output(string str)
+  {
+  }
+}
+
+class TestIOProvider : IOProvider
+{
+  string input()
+  {
+    return "6";
+  }
+
+  void output(string str)
+  {
+    writeln(str);
+  }
+}
+
+class Program
+{
+  private IOProvider io;
+
+  protected
+  {
+    string read()
+    {
+      return this.io.input();
+    }
+
+    void print(string str)
+    {
+      this.io.output(str);
+    }
+  }
+
+public:
+  this(IOProvider provider) nothrow
+  {
+    this.io = provider;
+  }
+
+  void main()
+  {
+    const string input = this.read();
+    const int n = input.to!int;
+
+    int[] l1 = map!(x => x % 6 + 1)(iota(1, n + 1)).array;
+    ulong[] l2 = map!(x => filter!(y => y == x)(l1).array.length)(iota(1, 7)).array;
+
+    const bool result = all!(x => x == l2[0])(l2);
+
+    this.print(result ? "Yes" : "No");
+  }
+}
+
+void main() nothrow
+{
+  try
+  {
+    auto ioProvider = new TestIOProvider();
+    new Program(ioProvider).main();
+  }
+  catch (Exception e)
+  {
+    return;
+  }
+}
