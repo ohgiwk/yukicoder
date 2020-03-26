568379a435360e3c1e4f3bc0712081a0fed4b13c
渡辺圭哉 <ohagi2149@gmail.com>
2020-03-27 01:30:29 +0900

    fix: inject IOProvider

--- 1003_サイコロの実装(1)/main.swift
+++ 1003_サイコロの実装(1)/main.swift
@@ -1,18 +1,81 @@
 
-func main() {
-  let input: String = readLine()!
-  // let input:String = "6"
-  let n:Int? = Int(input)
 
-  if let m = n {
-    var l:[Int] = (1...m).map({ $0 % 6 + 1 })
-    let match = { (x) -> [Int] in l.filter{ $0 == x } }
-    l = (1...6).map({ match($0).count })
+protocol IOProvider {
+  func input() -> String
+  func output(_ str: String) -> Void
+}
 
-
-    let result:Bool = l.allSatisfy({ $0 == l[0] })
-    print(result ? "Yes" : "No")
+class StandardIOProvider: IOProvider {
+  func input() -> String {
+    return readLine()!
   }
+
+  func output(_ str: String) -> Void {
+    return print(str)
+  }
+}
+
+class TestIOProvider: IOProvider {
+  func input() -> String {
+    return "6"
+  }
+
+  func output(_ str: String) -> Void {
+    print(str)
+  }
+}
+
+class FileIOProvider: IOProvider {
+  func input() -> String {
+    return ""
+  }
+
+  func output(_ str: String) -> Void {
+
+  }
+}
+
+class Base {
+  let io: IOProvider;
+
+  init(_ provider: IOProvider) {
+    io = provider
+  }
+
+  internal func read() -> String {
+    return io.input()
+  }
+
+  internal func write(_ str: String) -> Void {
+    io.output(str)
+  }
+
+  public func main() {}
+}
+
+
+class Program: Base {
+
+  public override func main() {
+    let input: String = self.read()
+    let n:Int? = Int(input)
+
+    if let m = n {
+      let l1:[Int] = (1...m).map({ $0 % 6 + 1 })
+      let match = { (x) -> [Int] in l1.filter{ $0 == x } }
+      let l2:[Int] = (1...6).map({ match($0).count })
+
+      let result:Bool = l2.allSatisfy({ $0 == l2[0] })
+
+      self.write(result ? "Yes" : "No")
+    }
+  }
+}
+
+
+func main() -> Void {
+  let provider = TestIOProvider()
+  Program(provider).main()
 }
 
 main()
