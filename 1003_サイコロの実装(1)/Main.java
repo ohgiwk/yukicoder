1fb0849052cecb640eadc89a744d24b422d45771
渡辺圭哉 <ohagi2149@gmail.com>
2020-04-13 23:57:20 +0900

    update: inject IOProvider class

--- 1003_サイコロの実装(1)/Main.java
+++ 1003_サイコロの実装(1)/Main.java
@@ -4,18 +4,83 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class Main {
 
-  public static void main(String[] args) {
+interface IOProvider {
+  public String read();
 
-    Scanner scanner = new Scanner(System.in);
+  public void write(String s);
+}
+
+
+class StdIOProvider implements IOProvider {
+  @Override
+  public String read() {
+    var scanner = new Scanner(System.in);
     String input = scanner.nextLine();
+    scanner.close();
+    return input;
+  }
+
+  @Override
+  public void write(String s) {
+    System.out.println(s);
+  }
+}
+
+
+class TestIOProvider implements IOProvider {
+  @Override
+  public String read() {
+    return "6";
+  }
+
+  @Override
+  public void write(String s) {
+    System.out.println(s);
+  }
+}
+
+
+class Base {
+  IOProvider io;
+
+  public Base(IOProvider provider) {
+    io = provider;
+  }
+
+  public String read() {
+    return "6";
+  }
+
+  public void write(String s) {
+    System.out.println(s);
+  }
+}
+
+
+final class Program extends Base {
+
+  public Program(IOProvider provider) {
+    super(provider);
+  }
+
+  public void main() {
+    String input = read();
 
     int n = Integer.parseInt(input);
-    long[] l = IntStream.range(1, n + 1).map(x -> x % 6 + 1).toArray();
-    l = IntStream.range(1, 7).map(x -> Arrays.stream(l).filter(y -> x == y).count()).toArray();
+    int[] l1 = IntStream.range(1, n + 1).map(x -> x % 6 + 1).toArray();
+    int[] l2 = IntStream.range(1, 7).map(x -> (int) Arrays.stream(l1).filter(y -> x == y).count())
+        .toArray();
+    boolean result = Arrays.stream(l2).allMatch(x -> x == l2[0]);
 
-    boolean result = Arrays.stream(l).allMatch(x -> x == l[0]);
-    System.out.println(result ? "Yes" : "No");
+    write(result ? "Yes" : "No");
+  }
+}
+
+
+public class Main {
+  public static void main(String[] args) {
+    var provider = new TestIOProvider();
+    new Program(provider).main();
   }
 }
