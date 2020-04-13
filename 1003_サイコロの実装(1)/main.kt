b38ab5bd9414638bd5429b48481857d3ce9e02a4
渡辺圭哉 <ohagi2149@gmail.com>
2020-04-14 00:39:07 +0900

    update: inject IOProvider class

--- 1003_サイコロの実装(1)/main.kt
+++ 1003_サイコロの実装(1)/main.kt
@@ -1,11 +1,60 @@
 
-fun main(): Unit {
-
-  val input: String = "6"
-  val n: Int = Integer.parseInt(input)
-  val l: List<Int> = (1..n).map { it % 6 + 1 }
-  val ll = (1..6).map { x -> l.filter { l -> x == l }.size }
-  val result: Boolean = ll.all { it == ll[0] }
-  print(if (result) "Yes" else "No")
+interface IOProvider {
+  fun input(): String
+  fun output(s: String): Unit
 }
 
+class StdIOProvider : IOProvider {
+  override fun input(): String {
+    return ""
+  }
+
+  override fun output(s: String): Unit {
+    print(s)
+  }
+}
+
+class TestIOProvider : IOProvider {
+  override fun input(): String {
+    return "6"
+  }
+
+  override fun output(s: String): Unit {
+    print(s)
+  }
+}
+
+abstract class Base(provider: IOProvider) {
+  val io = provider
+
+  protected fun read(): String {
+    return this.io.input()
+  }
+
+  protected fun write(str: String): Unit {
+    this.io.output(str)
+  }
+
+  open fun main() {}
+}
+
+class Program(provider: IOProvider): Base(provider) {
+
+
+  override fun main(): Unit {
+    val input: String = this.read()
+
+    val n: Int = Integer.parseInt(input)
+    val l1: List<Int> = (1..n).map { it % 6 + 1 }
+    val l2: List<Int> = (1..6).map { x -> l1.filter { l1 -> x == l1 }.size }
+    val result: Boolean = l2.all { it == l2[0] }
+
+    this.write(if (result) "Yes" else "No")
+  }
+
+}
+
+fun main(): Unit {
+  val provider = TestIOProvider()
+  Program(provider).main()
+}
