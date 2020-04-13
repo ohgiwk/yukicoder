4d289948ad8aa7552bcd3405f3c1e00e71990bfe
渡辺圭哉 <ohagi2149@gmail.com>
2020-04-13 23:58:03 +0900

    update: inject IOProvider class

--- 1003_サイコロの実装(1)/main.cs
+++ 1003_サイコロの実装(1)/main.cs
@@ -1,23 +1,104 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Program
+namespace Program
 {
-    public static void Main()
-    {
-        String input = Console.ReadLine();
-        // String input = "6";
-        int n = Convert.ToInt32(input);
-        // var n = 6;
-        IEnumerable<int> l = Enumerable.Range(1, n).Select(i => i % 6 + 1);
-        IEnumerable<int> l2 = Enumerable.Range(1, 6).Select(i => l.Where(j => i == j).Count());
-        bool result = l2.All(i => i == l2.ElementAt(0));
 
-        Console.WriteLine(result ? "Yes" : "No");
+    interface IOProvider
+    {
+        String Read();
+        // String ReadLine();
+        // String[] ReadLines();
+        void Write(String str);
+        // void WriteLn();
+    }
+
+    class TestIOProvider : IOProvider
+    {
+        public String Read()
+        {
+            return "6";
+        }
+
+        public void Write(String str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    class StdIOProvider : IOProvider
+    {
+        public String Read()
+        {
+            return Console.ReadLine();
+        }
+
+        public void Write(String str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    abstract class Base
+    {
+        protected IOProvider io;
+
+        public Base(IOProvider provider)
+        {
+            this.io = provider;
+        }
+
+        public String Read()
+        {
+            return this.io.Read();
+        }
+
+        public void Write(String str)
+        {
+            this.io.Write(str);
+        }
+
+        public virtual void Main() { }
+    }
+
+
+    class Program : Base
+    {
+        public Program(IOProvider provider) : base(provider)
+        {
+        }
+
+        public sealed override void Main()
+        {
+            String input = this.Read();
+
+            int n = Convert.ToInt32(input);
+            IEnumerable<int> l = Enumerable.Range(1, n).Select(i => i % 6 + 1);
+            IEnumerable<int> l2 = Enumerable.Range(1, 6).Select(i => l.Where(j => i == j).Count());
+            bool result = l2.All(i => i == l2.ElementAt(0));
+
+            this.Write(result ? "Yes" : "No");
+        }
+    }
+
+
+    public class Hello
+    {
+        public static void Main()
+        {
+            DateTime startTime = DateTime.Now;
+
+            var provider = new TestIOProvider();
+            new Program(provider).Main();
+
+            DateTime endTime = DateTime.Now;
+            TimeSpan diff = endTime - startTime;
+            Console.WriteLine($"code runned in {diff}");
+        }
     }
 }
 
-
-// Program.Main();
+// Program.Hello.Main();
