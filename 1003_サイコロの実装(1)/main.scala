de412a26b140940a32636767f8d0eba63d29f0b8
渡辺圭哉 <ohagi2149@gmail.com>
2020-04-13 23:55:44 +0900

    update: inject IOProvider class

--- 1003_サイコロの実装(1)/main.scala
+++ 1003_サイコロの実装(1)/main.scala
@@ -1,14 +1,63 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    val input: String = io.stdIn.readLine().split()
-    // val input: String = "6"
-    val n: Int = input.toInt
 
+trait IOProvider {
+  def read(): String
+  def write(str: String): Unit
+}
+
+
+class StdIOProvider extends IOProvider {
+  def read(): String = {
+    return io.StdIn.readLine()
+  }
+
+  def write(str: String): Unit = {
+    println(str)
+  }
+}
+
+class TestIOProvider extends IOProvider {
+  def read(): String = {
+    return "6"
+  }
+
+  def write(str: String): Unit = {
+    println(str)
+  }
+}
+
+
+abstract class Base(provider: IOProvider) {
+  val io: IOProvider = provider;
+
+  def read(): String = {
+    return io.read()
+  }
+
+  def write(str: String): Unit = {
+    io.write(str)
+  }
+
+  def main(): Unit = {}
+}
+
+
+class Program(provider: IOProvider) extends Base(provider) {
+  override def main(): Unit = {
+    val input: String = this.read()
+
+    val n: Int = input.toInt
     val l: List[Int] = (1 to n).toList.map(_ % 6 + 1)
     val l2: List[Int] = (1 to 6).toList.map(i => l.filter(j => i == j).size)
-
     val result: Boolean = l2.forall(_ == l2.head)
 
-    println(if (result) "Yes" else "No")
+    this.write(if (result) "Yes" else "No")
+  }
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val provider = new TestIOProvider()
+    new Program(provider).main()
   }
 }
